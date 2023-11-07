d9ba5956dd696600fd92e46a2e1aa5b43b2fde57
Maurits Bos <mb.brains@gmail.com>
2023-11-07 13:11:05 +0300

    added error handling

--- repotofile/cli.py
+++ repotofile/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from utils import sanitize_config_files, create_output_file
 from git_operations import clone_repo
 
@@ -12,13 +13,23 @@ def main():
     args = parser.parse_args()
 
     # Step 1: Clone the repo
-    repo_dir = 'repo_dir'  # This would be a temporary directory created for cloning
-    if clone_repo(args.url, args.branch, repo_dir):
+    try:
+        repo_dir = 'repo_dir'  # This would be a temporary directory created for cloning
+        success = clone_repo(args.url, args.branch, repo_dir)
+        if not success:
+            raise Exception("Failed to clone the repository. Please check the URL and branch/tag name.")
+        
         # Step 2 (optional): Sanitize configuration files if --no-config is set
         if args.no_config:
             sanitize_config_files(repo_dir)
-        # Steps for generating the output file will go here
+
+        # Step 3: Generate the output file
         create_output_file(repo_dir, args.output)
-        
+
+        print(f"Repository contents have been successfully written to {args.output}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}", file=sys.stderr)
+        sys.exit(1)
 if __name__ == "__main__":
     main()
