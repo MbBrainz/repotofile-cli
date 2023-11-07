05d8a8d2cafcfddffc11e1755487942d9001f20b
Maurits Bos <mb.brains@gmail.com>
2023-11-07 13:41:06 +0300

    fixed encoding bug and branch bug

--- repotofile/git_operations.py
+++ repotofile/git_operations.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def clone_repo(url, branch='master', output_dir='repo_dir'):
+def clone_repo(url, branch=None, output_dir='repo_dir'):
     # Clone the repository into the specified directory
     clone_command = ['git', 'clone', '--depth', '1']
     if branch:  # If a specific branch or tag is given, use it
--- repotofile/utils.py
+++ repotofile/utils.py
@@ -5,15 +5,25 @@ import os
 CONFIG_PATTERNS = ['*.yaml', '*.yml', '*.json', '*.conf', '*.ini', '*.env']
 
 def create_output_file(repo_dir, output_file):
+    common_encodings = ['utf-8', 'latin-1', 'utf-16']
     with open(output_file, 'w', encoding='utf-8') as output:
         for root, dirs, files in os.walk(repo_dir):
             for file in files:
                 file_path = os.path.join(root, file)
-                output.write(f'--- Start of {file_path} ---\n')
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    output.write(f.read())
-                output.write(f'--- End of {file_path} ---\n\n')
-
+                file_content = None
+                for encoding in common_encodings:
+                    try:
+                        with open(file_path, 'r', encoding=encoding) as f:
+                            file_content = f.read()
+                        break  # if the file is read successfully, exit the loop
+                    except UnicodeDecodeError:
+                        continue  # if an error occurs, try the next encoding
+                if file_content is not None:
+                    output.write(f'--- Start of {file_path} ---\n')
+                    output.write(file_content)
+                    output.write(f'--- End of {file_path} ---\n\n')
+                else:
+                    print(f"Skipping file {file_path} due to encoding issues.")
 
 def sanitize_config_files(repo_dir):
     for root, dirs, files in os.walk(repo_dir):
